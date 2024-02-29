1c4c60bfc1e140b8c10fe17275353a5d3c666c09
Santiago Melo <santiagocano15cr7@gmail.com>
2024-02-29 15:28:45 -0600

    Añadiendo un componente llamado ComponentMain donde damos todas las secciones

---
+++ src/layout/ComponentMain.jsx
@@ -0,0 +1,41 @@
+import {
+  Model3,
+  ModelS,
+  ModelY,
+  ModelX,
+  VideoExperiencie,
+  Powerwall,
+  Acessories,
+} from "../components/sections/index";
+import { Menu } from "../components/Menu";
+
+export const ComponentMain = () => {
+  return (
+    <>
+      <Menu />
+      <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth">
+        <div className="snap-center">
+          <Model3 />
+        </div>
+        <div className="snap-center">
+          <ModelY />
+        </div>
+        <div className="snap-center">
+          <ModelX />
+        </div>
+        <div className="snap-center">
+          <ModelS />
+        </div>
+        <div className="snap-center">
+          <VideoExperiencie />
+        </div>
+        <div className="snap-center">
+          <Powerwall />
+        </div>
+        <div className="snap-center">
+          <Acessories />
+        </div>
+      </main>
+    </>
+  );
+};
