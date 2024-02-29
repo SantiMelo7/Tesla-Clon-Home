83c5b94ea39d5e17b142cedbb1525464b60484e8
Santiago Melo <santiagocano15cr7@gmail.com>
2024-02-29 17:18:17 -0600

    Añadiendo el hover de tesla correctamente

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -4,14 +4,14 @@ import LanguageIcon from "@mui/icons-material/Language";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SvgResponsive from "../responsive/SvgResponsive";
-import { ListMenu, HoverDiv } from "../../conventional/index";
+import { ListMenu } from "../../conventional/index";
 
 export const Menu = () => {
   return (
     <header className="px-10 py-10 md:-top-6 sm:-left-6 flex items-center w-full fixed justify-between z-50 animate-slide-in-top">
       <SvgResponsive />
       <nav>
-        <ul className="md:flex sm:hidden gap-x-8 text-sm [&>li]: font-extrabold">
+        <ul className="lg:flex sm:hidden gap-x-8 text-sm [&>li]: font-extrabold">
           <ListMenu text="Vehículos" />
           <ListMenu text="Energía" />
           <ListMenu text="Carga" />
@@ -20,7 +20,7 @@ export const Menu = () => {
         </ul>
       </nav>
       <nav className="flex flex-grow justify-end right-2 basis-0 relative -top-[3px]">
-        <ul className="md:flex sm:hidden gap-x-8 text-sm [&>li]: font-extrabold">
+        <ul className="lg:flex sm:hidden gap-x-8 text-sm [&>li]: font-extrabold">
           <ListMenu
             text={
               <LanguageIcon className="w-[20px] h-[20px] border-[#171a20] text-[#171a20]" />
@@ -38,7 +38,6 @@ export const Menu = () => {
           />
         </ul>
       </nav>
-      <HoverDiv />
     </header>
   );
 };
--- src/conventional/ListMenu.jsx
+++ src/conventional/ListMenu.jsx
@@ -1,11 +1,20 @@
 import { useHover } from "../useHover";
 
 export const ListMenu = ({ text }) => {
-  const { handleMouseEnter, handleMouseLeave } = useHover();
+  const { menuStyles, handleMouseEnter, handleMouseLeave } = useHover();
 
   return (
-    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {text}
-    </li>
+    <>
+      <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {text}
+      </li>
+      <div className="absolute top-[18px] left-[23px] right-0">
+        <div
+          className="absolute bg-black/5 backdrop-blur-lg transition-all duration-500
+          ease-in-out opacity-0 -z-10 py-4 "
+          style={menuStyles}
+        ></div>
+      </div>
+    </>
   );
 };
