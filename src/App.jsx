f1357a2c7e23c2d1ece86cc1ff1137b4dbb97104
Santiago Melo <santiagocano15cr7@gmail.com>
2024-02-29 15:26:49 -0600

    Quotando todos los componentes y dando el componente principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,5 @@
-import Menu from "./components/Menu";
-import Acessories from "./components/sections/Acessories";
-import Model3 from "./components/sections/Model3";
-import ModelS from "./components/sections/ModelS";
-import ModelX from "./components/sections/ModelX";
-import ModelY from "./components/sections/ModelY";
-import Powerwall from "./components/sections/Powerwall";
-import VideoExperiencie from "./components/sections/VideoExperiencie";
+import { ComponentMain } from "./layout/index";
 
-export function App() {
-  return (
-    <>
-      <Menu />
-      <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth">
-        <div className="snap-center">
-          <Model3 />
-        </div>
-        <div className="snap-center">
-          <ModelY />
-        </div>
-        <div className="snap-center">
-          <ModelX />
-        </div>
-        <div className="snap-center">
-          <ModelS />
-        </div>
-        <div className="snap-center">
-          <VideoExperiencie />
-        </div>
-        <div className="snap-center">
-          <Powerwall />
-        </div>
-        <div className="snap-center">
-          <Acessories />
-        </div>
-      </main>
-    </>
-  );
-}
+export const App = () => {
+  return <ComponentMain />;
+};
