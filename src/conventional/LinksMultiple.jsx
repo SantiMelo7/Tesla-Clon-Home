dc57a1a9beb7098e9eb83ff730c0d52bc150a20c
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-01 15:24:11 -0600

    Añadiendo un nuevo componente para manejar los botones

---
+++ src/conventional/LinksMultiple.jsx
@@ -0,0 +1,12 @@
+export default function LinksMultiple({ text, className }) {
+  return (
+    <a
+      className={` inline-block py-2 px-14 text-sm rounded-[4px] font-normal
+        shadow-lg shadow-[#333] ${className}`}
+      target="_blank"
+      rel="noopener norreferer"
+    >
+      {text}
+    </a>
+  );
+}
--- src/conventional/index.js
+++ src/conventional/index.js
@@ -1,2 +1,3 @@
 export * from "./ComponentMain"
 export * from "./ListMenu"
+export * from "./LinksMultiple"
