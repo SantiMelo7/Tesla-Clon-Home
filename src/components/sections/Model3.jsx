d5b203beb18bc976bf7607c638ff856e81d8be8f
Santiago Melo <santiagocano15cr7@gmail.com>
2024-06-05 15:16:27 -0600

    Animations

--- src/components/sections/Model3.jsx
+++ src/components/sections/Model3.jsx
@@ -4,7 +4,7 @@ import Image from "../images/Image";
 
 export const Model3 = () => {
   return (
-    <section className="text-center h-screen w-screen relative">
+    <section className="text-center h-screen w-screen relative srcolled">
       <Section title="Model 3" className="text-black animate-slide-in-bottom">
         <div className="flex flex-col flex-grow justify-end mb-[45px] font-extrabold">
           <LinkDouble />
--- src/conventional/ComponentMain.jsx
+++ src/conventional/ComponentMain.jsx
@@ -8,13 +8,40 @@ import {
   Acessories,
 } from "../components/sections/index";
 import { Footer, Menu } from "../components/layout/index";
+import { useEffect, useRef } from "react";
 
 export const ComponentMain = () => {
+  const aboutContentRef = useRef(null);
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+    observerRef.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("scrolled");
+        }
+      });
+    }, options);
+    const currentObserver = observerRef.current;
+    if (aboutContentRef.current) {
+      currentObserver.observe(aboutContentRef.current);
+    }
+    return () => {
+      if (currentObserver && aboutContentRef.current) {
+        currentObserver.unobserve(aboutContentRef.current);
+      }
+    };
+  }, [aboutContentRef]);
   return (
     <>
       <Menu />
       <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth">
-        <div className="snap-center">
+        <div className="snap-center" ref={aboutContentRef}>
           <Model3 />
         </div>
         <div className="snap-center">
